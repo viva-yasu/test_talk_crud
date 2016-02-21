b2104e2acd28deb73f37017145961eae8a4d2cd8
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-21 18:17:15 +0900

    talk CRUD

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,11 +1,27 @@
 class TalksController < ApplicationController
   def index
     @talks = Talk.all
+    @talk = Talk.new
   end
 
   def create
+    talk = Talk.new(talk_params)
+    talk.save
+    redirect_to talks_index_path
   end
 
   def destroy
+    talk = Talk.find_by(id: params[:talk][:id])
+    if !talk
+      redirect_to talks_index_path
+    end
+    talk.destroy
+    redirect_to talks_index_path
   end
+
+  private
+  def talk_params
+    params.require(:talk).permit(:message)
+  end
+
 end
