61568977eff3c20ad79f6852ea497ae5cbfa80f8
Yasunari Kondo <kyasunari123v@gmail.com>
2016-02-21 23:00:09 +0900

    add user

--- app/controllers/talks_controller.rb
+++ app/controllers/talks_controller.rb
@@ -1,20 +1,25 @@
 class TalksController < ApplicationController
   def index
     @talks = Talk.all
-    @talk = Talk.new
+    @new_talk = Talk.new
     @current_login_user_id = 1
+    @talk = Talk.new
   end
 
   def create
-    talk = Talk.new(talk_params)
-    talk.save
-    redirect_to talks_index_path
+    @talk = Talk.new(talk_params)
+    if @talk.save
+      redirect_to talks_index_path
+    else
+      before_render
+      render 'index'
+    end
   end
 
   def destroy
     talk = Talk.find_by(id: params[:talk][:id])
-    if !talk
-      redirect_to talks_index_path
+    if talk.nil?
+      return redirect_to talks_index_path
     end
     talk.destroy
     redirect_to talks_index_path
@@ -23,6 +28,12 @@ class TalksController < ApplicationController
   private
   def talk_params
     params.require(:talk).permit(:message, :user_id)
+  end
+
+  def before_render
+    @talks = Talk.all
+    @new_talk = Talk.new
+    @current_login_user_id = 1
   end
 
 end
